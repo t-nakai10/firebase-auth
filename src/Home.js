e780e0da7a07e5e0f1439b6ca95d020ae0ee003e
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-19 13:59:21 +0900

    サインインボタンを作成していく

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Home() {
-  return <div>ログイン</div>;
+  return (
+    <div>
+      <SignInButton />
+    </div>
+  );
 }
 
 export default Home;
+
+// コンポーネントではなく一旦同ファイルに作成.
+function SignInButton() {
+  const signInWithGoogle = () => {
+    // firebase とグールグル
+  };
+
+  return <button onClick={signInWithGoogle}>グーグルでサインイン</button>;
+}
